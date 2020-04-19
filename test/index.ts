c4d39765eb77d4a132d668f5cfbeb1be2a3370e8
Yangyan <690243003@qq.com>
2020-04-19 20:22:08 +0800

    feat: 测试框架算是搭建好了 但是watch模式不管用

---
+++ test/index.ts
@@ -0,0 +1,20 @@
+describe("aaaa",function(){
+  it("ccc",function() {
+    
+  })
+  it("ccc",function() {
+    
+  })
+  it("ccc",function() {
+    
+  })
+  it("bbb",function() {
+    
+  })
+  it("bbb",function() {
+    
+  })
+  it("eee",function() {
+    
+  })
+})
